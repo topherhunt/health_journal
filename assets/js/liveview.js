2c562aad86e9a2a8b07e85650453962014d514ef
Topher Hunt <hunt.topher@gmail.com>
2020-03-10 13:37:26 +0100

    Fix latency bug when editing forms

--- assets/js/liveview.js
+++ assets/js/liveview.js
@@ -4,3 +4,7 @@ import LiveSocket from "phoenix_live_view"
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
 liveSocket.connect()
+
+// For latency simulation.
+// See https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#module-simulating-latency
+window.liveSocket = liveSocket
